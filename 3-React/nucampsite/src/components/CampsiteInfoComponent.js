8be2dcc1eb7e49a7c333133bd2146689baac7dd7
jobieSenor <jobiesenor@gmail.com>
2020-08-19 09:58:24 -0700

    Header and Footer

--- 3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ 3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
-class CampsiteInfo extends Component {
-   
-   renderComments(comments) {
+
+function RenderCampsite({campsite}) {
+    return (
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    )
+}
+
+  function RenderComments({comments}) {
        if(comments) {
            return (
                <div className="col-md-5 m-1">
@@ -24,27 +37,13 @@ class CampsiteInfo extends Component {
        return <div />;
    }
 
-   renderCampsite(campsite) {
-        return (
-            <div className="col-md-5 m-1">
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
-        )
-   }
-
-   render() {
-        if (this.props.campsite) {
+   function CampsiteInfo(props) {
+        if (props.campsite) {
             return (
                 <div className="container">
                     <div className="row">
-                            {this.renderCampsite(this.props.campsite)}
-                            {this.renderComments(this.props.campsite.comments)}
+                            <RenderCampsite campsite={props.campsite}  />
+                            <RenderComments comments={props.campsite.comments} />
                     </div>
                 </div>
                 
@@ -56,6 +55,6 @@ class CampsiteInfo extends Component {
 
     
     };
-}
+
 
 export default CampsiteInfo;
